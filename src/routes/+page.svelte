adfd253df428a532df7ecfe3c2c937f5a32f744f
Jack Hogan <jackhogan11@gmail.com>
2024-11-23 03:36:25 -0500

    Worked on auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { signIn } from '@auth/sveltekit/client';
+</script>
+
+<h1 class="text-4xl">➡️🦑⬅️</h1>
+<p>SQUISH some links with IMSQU.ID</p>
+
+<div>
+	<button onclick={(_) => signIn()}>SIGN IN</button>
+</div>
--- src/routes/[slug]/+server.ts
+++ src/routes/[slug]/+server.ts
@@ -1,5 +1,10 @@
 import { error, redirect, type RequestEvent } from '@sveltejs/kit';
 
+interface Value {
+	target: string;
+	hits: number;
+}
+
 export async function GET({ url, platform }: RequestEvent) {
 	const stripped = url.pathname.replaceAll('/', '');
 	console.log(stripped);
@@ -7,10 +12,23 @@ export async function GET({ url, platform }: RequestEvent) {
 	console.log(await kv.get(stripped));
 	console.log(await kv.list());
 
-	const value = await kv.get(stripped);
+	const value: Value | null = await kv.get(stripped, 'json');
 	if (value) {
-		return redirect(302, value);
+		value.hits += 1;
+		await kv.put(stripped, JSON.stringify(value));
+		return redirect(302, value.target);
 	}
 
 	return error(404, 'Invalid short link');
 }
+
+export async function PUT({ locals }: RequestEvent) {
+	const session = await locals.auth();
+	if (!(session && session.user)) {
+		return error(401, 'No!!!!!!!!! You are not authorized!!!!!!');
+	}
+
+	const user = session.user;
+}
+
+export async function DELETE({ locals }: RequestEvent) {}
