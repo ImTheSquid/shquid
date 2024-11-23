833c5e312cd47987cacf4d5ce8e2d2734bfca24b
Jack Hogan <jackhogan11@gmail.com>
2024-11-23 03:56:57 -0500

    Observability, trust host

--- src/auth.ts
+++ src/auth.ts
@@ -2,5 +2,6 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/sveltekit/providers/github';
 
 export const { handle, signIn } = SvelteKitAuth({
-	providers: [GitHub]
+	providers: [GitHub],
+	trustHost: true
 });
--- src/routes/[slug]/+server.ts
+++ src/routes/[slug]/+server.ts
@@ -6,11 +6,16 @@ interface Value {
 }
 
 export async function GET({ url, platform }: RequestEvent) {
-	const stripped = url.pathname.replaceAll('/', '');
+	const stripped = url.pathname.substring(1);
 	console.log(stripped);
 	const kv = platform?.env.SHQUID_KV as KVNamespace;
 	console.log(await kv.get(stripped));
 	console.log(await kv.list());
+
+	// Handle special paths for Auth.js
+	if (stripped === 'auth' || stripped.startsWith('auth/callback')) {
+		throw new Error('This route should not be handled here.'); // Or redirect as needed
+	}
 
 	const value: Value | null = await kv.get(stripped, 'json');
 	if (value) {
