2c03605426416f08a7782e87706906877624d556
Jack Hogan <jackhogan11@gmail.com>
2025-03-31 14:29:32 +0100

    Should support arbitrary routes now

--- src/routes/[slug]/[...extra]/+server.ts
+++ src/routes/[slug]/[...extra]/+server.ts
@@ -2,7 +2,7 @@ import { getLinksCollection, type Link } from '$lib';
 import { error, redirect, type RequestEvent } from '@sveltejs/kit';
 import type { WithId } from 'mongodb';
 
-export async function GET({ params: { slug } }: RequestEvent) {
+export async function GET({ params: { slug, extra } }: RequestEvent) {
 	const stripped = slug;
 
 	const coll = await getLinksCollection();
@@ -10,7 +10,7 @@ export async function GET({ params: { slug } }: RequestEvent) {
 	const value = (await coll.findOne({ key: stripped })) as WithId<Link> | null;
 	if (value) {
 		await coll.findOneAndUpdate({ _id: value._id }, { $inc: { hits: 1 } });
-		return redirect(302, value.target);
+		return redirect(302, value.target + '/' + extra);
 	}
 
 	error(404, 'Invalid short link');
